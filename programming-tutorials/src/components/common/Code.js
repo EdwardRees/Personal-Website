4b99f7e8bece29b2cc5f5e346b2b89eaca5ce930
Edward Rees <erees.hk@gmail.com>
2018-12-31 16:28:24 +0800

    deploy

--- programming-tutorials/src/components/common/Code.js
+++ programming-tutorials/src/components/common/Code.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Code extends Component {
   render() {
-    return <code style={{height: 100, backgroundColor: rgba(0,0,0,0.5)}}>{this.props.children}</code>;
+    return <code style={{height: 100}}>{this.props.children}</code>;
   }
 }
 

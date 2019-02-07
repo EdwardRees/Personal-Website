2df8430b3fddfdce3fe72a2040651f87f804638d
EdwardR1 <erees.hk@gmail.com>
2019-02-07 13:46:03 -0800

    Change of file location

---
+++ ChainDimennxion/js/header.js
@@ -0,0 +1,5 @@
+$("document").ready(() => {
+  $("#header").html(
+    "<nav class='navbar navbar-toggleable-sm navbar-expand-lg navbar-light'><a class='navbar-brand' href='./index.html' style='color:#B3A479'>Chain Dimennxion</a><button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#menu' aria-controls='menu'aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button><div class='collapse navbar-collapse justify-content-between' id='menu'><div class='navbar-nav'></div><div class='navbar-nav'><a href='./index.html' class='nav-item nav-link' style='color: #B3A479'>Home</a><a href='./edge.html' class='nav-item nav-link' style='color: #B3A479'>Edge</a><a href='./404.html' class='nav-item nav-link' style='color: #B3A479'>Beliefs</a><a href='./404.html' class='nav-item nav-link' style='color: #B3A479'>Projects</a><a href='./404.html' class='nav-item nav-link' style='color: #B3A479'>Contact</a></div></div></nav>"
+  );
+});
